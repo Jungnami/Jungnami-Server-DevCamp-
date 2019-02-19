582aa3c3de4d7babb9d494ecb2ac2711715f4e6f
ihaeeun <ihaeeun16@gmail.com>
2019-02-19 12:56:27 +0900

    Modify admin.js, detail.js (국회의원 상세 정보 수정 제외)

--- pms/routes/api/admin/admin.js
+++ pms/routes/api/admin/admin.js
@@ -13,39 +13,51 @@ const { detail: detail } = require('../../../parser/detail');
 const router = express.Router();
 
 // pms admin page
+// select option에 필요한 데이터 전달
 router.get('/pms', async(req, res, next) => {
     let resultArr = new Array();
 
-    const selectPartyQuery = 'SELECT party_name FROM party';
+    const selectPartyQuery = 'SELECT party_cd FROM party';
     const selectPartyResult = await db.queryParam_None(selectPartyQuery);
-    const selectCityQuery = 'SELECT city_name FROM city';
+    const selectCityQuery = 'SELECT city_cd FROM city';
     const selectCityResult = await db.queryParam_None(selectCityQuery);
     const selectOrdinalQuery = 'SELECT ordinal FROM legislator GROUP BY ordinal';
     const selectOrdinalResult = await db.queryParam_None(selectOrdinalQuery);
     
     resultArr.push(selectPartyResult, selectCityResult, selectOrdinalResult);
+    console.log(resultArr)
 
     if(!selectPartyResult) {
         res.status(statusCode.OK).send(pmsUtil.successFalse(null, responseMessage.DB_ERROR, statusCode.DB_ERROR));
     } else {
-        // res.sendFile(path.join(__dirname, '../../../public/index.html'))
         res.status(statusCode.OK).send(pmsUtil.successTrue(responseMessage.PMS_ADMIN_LOAD_SUCCESS, resultArr));
     }
 });
 
 
 // **조회 요청시 rest 어떻게 보내고, 받은 정보 어떻게 사용하는지 where???
+// select box에서 받아온 여러 개 값 확인하는 방법
+// default로 최신 대수
+//req.body.search-legislator, body-parser 이용
+router.post('/pms/search', async(req, res, next) => {
+    const selectBox = new Array();
+    for (let i in Object.keys(req.body.data)) {
+        if (req.body.data[i] == null) {
+            req.body.data[i] = '%';
+        }
+        selectBox.push(req.body.data[i]);
+    }
 
-router.get('/pms/search', async(req, res, next) => {
-    // const selectLegiQuery = 'SELECT legi_name, party_cd, city_cd, ordinal, crime FROM legislator WHERE party_cd=?, city_cd=?, ordinal=?, crime=?';
-    const selectLegiQuery = 'SELECT legi_name, party_cd, city_cd, ordinal, crime, profile_img FROM legislator';
-    const selectLegiResult = await db.queryParam_None(selectLegiQuery);
+    const selectLegiQuery = 'SELECT idx, legi_name, profile_img FROM legislator WHERE legi_name LIKE ? AND party_cd LIKE ? AND city_cd LIKE ? AND ordinal LIKE ?';
+    const selectLegiResult = await db.queryParam_Arr(selectLegiQuery, selectBox);
+    console.log(selectBox)
+    console.log(selectLegiQuery)
+    console.log(selectLegiResult)
     if(!selectLegiResult) {
         res.status(statusCode.OK).send(pmsUtil.successFalse(null, responseMessage.DB_ERROR, statusCode.DB_ERROR));
     } else {
         res.status(statusCode.OK).send(pmsUtil.successTrue(responseMessage.PMS_ADMIN_SEARCH_SUCCESS, selectLegiResult))
     }
-
 })
 
 // Initial insert
@@ -63,8 +75,7 @@ router.post('/insert', async(req, res, next) => {
 
 //api update
 router.put('/update', async(req, res, next) => {
-
+    
 })
-
 
 module.exports = router;
