76305b58a02cdcae6ba5d299442b7a4f99cf2901
ihaeeun <ihaeeun16@gmail.com>
2019-02-21 10:09:50 +0900

    modify routes

--- pms/routes/index.js
+++ pms/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/list', require('./api/politician/list'));
 router.use('/ranking', require('./api/politician/ranking'));
 router.use('/legislator', require('./api/politician/legislator'));
-
-router.use('/admin', require('./api/admin/admin'));
+router.use('/pms', require('./api/admin/pms'));
+router.use('/detail', require('./api/admin/detail'));
 
 module.exports = router;
