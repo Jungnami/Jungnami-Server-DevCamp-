492151cc429445d0ca543b358fecceb4446906f3
김현진 <hyunjk.luz@gmail.com>
2019-02-21 21:11:09 +0900

    update success message null

--- auth/routes/api/auth/login.js
+++ auth/routes/api/auth/login.js
@@ -7,7 +7,17 @@ const statusCode = require('../../../../commons/utils/statusCode');
 const passport = require('passport')
 
 //naver 로그인
-router.get('/naver', passport.authenticate('naver'));
+router.post('/naver', async (req, res) => {
+    // kakao access token
+    let accessToken = req.body.accessToken;
+    if (!accessToken) {
+        res.status(200).send(authUtil.successFalse(resposeMessage.AUTH, statudCode.AUTH_OK));
+    }
+
+    // push 알람 클라이언트 토큰
+    let fcmToken = req.body.fcmToken;
+
+});
 
 //naver login 연동 콜백
 router.get('/naver/callback', async (req, res) => {
@@ -34,5 +44,6 @@ router.get('/fail', async (req, res) => {
 router.get('/success', async (req, res) => {
     res.status(200).send(authUtil.successTrue(statusCode.AUTH_OK, responseMessage.LOGIN_SUCCESS, req._passport.session.user));
 });
+
 
 module.exports = router;
