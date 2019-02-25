e554b218ea8b1761a062dd0b2b76984522db3a53
ihaeeun <ihaeeun16@gmail.com>
2019-02-26 01:08:52 +0900

    좋아요 취소 수정

--- pms/routes/api/politician/like.js
+++ pms/routes/api/politician/like.js
@@ -14,18 +14,27 @@ router.post('/', authUtil.isLoggedin, async (req, res, next) => {
 
     const selectLikeQuery = 'SELECT * FROM legislator_comment_like WHERE user_idx=? AND comment_idx=?'
     const selectLikeResult = await db.queryParam_Arr(selectLikeQuery, [req.decoded.idx, reply_idx])
-    // console.log(selectLikeResult)
+
     if(!selectLikeResult) {
         res.status(200).send(authUtil.successFalse(responseMessage.REPLY_LIKE_READ_ERROR, statusCode.REPLY_LIKE_DB_ERROR))
     } else if(selectLikeResult.length === 1){
-        if(isLike==='1') {
-            resMsg = responseMessage.REPLY_LIKE_ALREADY;
+        const deleteLikeQuery = 'DELETE FROM legislator_comment_like WHERE user_idx = ? AND comment_idx = ? AND like_flag = ?';
+        let deleteLikeResult;
+
+        if(isLike === '1') {
+            deleteLikeResult = await db.queryParam_Arr(deleteLikeQuery, [req.decoded.idx, reply_idx, isLike]);
         } else {
-            resMsg = responseMessage.REPLY_DISLIKE_ALREADY;
+            deleteLikeResult = await db.queryParam_Arr(deleteLikeQuery, [req.decoded.idx, reply_idx, isLike]);
         }
-        res.status(200).send(authUtil.successTrue(statusCode.REPLY_BAD_REQUEST, resMsg));
+
+        if (!deleteLikeResult) {
+            res.status(200).send(authUtil.successFalse(responseMessage.REPLY_LIKE_CANCEL_ERROR, statusCode.REPLY_LIKE_DB_ERROR));        
+        } else {
+            res.status(200).send(authUtil.successTrue(statusCode.REPLY_LIKE_OK, responseMessage.REPLY_LIKE_CANCEL_OK));
+        }
+
     } else {
-        if(isLike) {
+        if(isLike === '1') {
             resMsg = responseMessage.REPLY_LIKE_LIKE_ERROR;
         } else {
             resMsg = responseMessage.REPLY_LIKE_DISLIKE_ERROR;
@@ -41,19 +50,5 @@ router.post('/', authUtil.isLoggedin, async (req, res, next) => {
         }
     }
 });
-
-router.delete('/', authUtil.isLoggedin, async (req, res) => {
-    const isLike = req.body.isLike;
-    const reply_idx = req.body.reply_idx;
-
-    var deleteLikeQuery = 'DELETE FROM legislator_comment_like WHERE user_idx = ? AND comment_idx = ? AND like_flag = ?';
-    let deleteLikeResult = await db.queryParam_Arr(deleteLikeQuery, [req.decoded.idx, reply_idx, isLike]);
-console.log(deleteLikeResult)
-    if (!deleteLikeResult) {
-        res.status(200).send(authUtil.successFalse(responseMessage.REPLY_LIKE_CANCEL_ERROR, statusCode.REPLY_LIKE_DB_ERROR));        
-    } else {
-        res.status(200).send(authUtil.successTrue(statusCode.REPLY_LIKE_OK, responseMessage.REPLY_LIKE_CANCEL_OK));
-    }
-}); 
 
 module.exports = router;
